bccebfd610a7faf20b85c3e51551253fc5c9125e
valentinacorreia <e227277@edu.sbl.ch>
2024-10-25 11:57:54 +0000

    Aufgabe 1 fertig gemacht

--- script.js
+++ script.js
@@ -41,3 +41,21 @@ export function aufgabe01(args) {
 }
 
 linkupExerciseHandler("[data-click=aufgabe01]", aufgabe01)
+
+export function aufgabe02(args) {
+  const input = args
+  const result = [] // Das ist die Resultatsliste
+
+  // Läuft Zeuchen für Zeichen über den ganzen Text
+  for (let i = 0; i < input.length; i++) {
+    const currentElement = input[i]
+
+    // Hänge das aktuelle Zeichen doppelt an
+    result.push(currentElement)
+    result.push(currentElement)
+  }
+
+  return result.join("")
+}
+
+linkupExerciseHandler("[data-click=aufgabe02]", aufgabe02)
