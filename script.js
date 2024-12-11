b12236d9dcbc8d0f616c2cbdceea3fa61cf77e5f
valentinacorreia <e227277@edu.sbl.ch>
2024-12-11 12:59:09 +0000

    aufgabe 17 verändert

--- script.js
+++ script.js
@@ -307,7 +307,7 @@ export function aufgabe17(args) {
   for (let i = 0; i < input.length; i++) {
     const currentElement = input[i]
     //Wenn wir auf ein Leerzeichen treffen, dann schreiben wir alles was wir bis jetzt haben, in die totalList
-    if (currentElement === " ") {
+    if (currentElement === ",") {
       totalList.push(currentList.join(""))
       currentList.length = 0
     } else {
